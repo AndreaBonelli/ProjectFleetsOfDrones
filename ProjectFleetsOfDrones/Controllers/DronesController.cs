63ed2ec9561fb31ced387b08cbf87cd81f951dd3
Andrea Bonelli <bonelliandrea97@gmail.com>
2022-10-17 14:42:25 +0200

    Created Add (Post)

--- ProjectFleetsOfDrones/Controllers/DronesController.cs
+++ ProjectFleetsOfDrones/Controllers/DronesController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjectFleetsOfDrones.Models;
+using System.IO;
+using System.Text;
 
 namespace ProjectFleetsOfDrones.Controllers
 {
@@ -6,7 +9,17 @@ namespace ProjectFleetsOfDrones.Controllers
     [Route("[controller]")]
     public class DronesController : ControllerBase
     {
-       
+        [HttpPost]
+        public IActionResult Add([FromBody] Drone drone)
+        {
+            Write(drone.ToString());
+            return Ok(drone);
+        }
+
+        public static void Write(string s)
+        {
+            System.IO.File.AppendAllText("Drones.txt", s);
+        }
 
     }
 }
--- ProjectFleetsOfDrones/Models/Drone.cs
+++ ProjectFleetsOfDrones/Models/Drone.cs
@@ -17,5 +17,10 @@
         public TimeSpan FlightTime { get; set; }
         public PropulsionType Propulsion { get; set; }
         public PilotType Pilot { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Drone Id: {DroneId}, Flight time: {FlightTime}, Propulsion type: {Propulsion}, Pilot type: {Pilot}";
+        }
     }
 }
